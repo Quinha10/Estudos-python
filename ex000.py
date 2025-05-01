df0b029a54bf40ecb6d95d3ad7831ef9e3c995bb
Quinha10 <lucasduarte2608@gmail.com>
2025-05-01 02:23:29 -0300

    Calcular media

--- ex000.py
+++ ex000.py
@@ -1,5 +1,11 @@
  #Ex-soma-python
-n1 = int (input('numero1:'))
-n2 = int (input('numero2:'))
-soma = n1 + n2
-print ('resultado', soma)
+n1 = float (input('digite a nota1:'))
+n2 = float (input('digita a nota2:'))
+n3 = float (input('digita a nota3:'))
+
+media = (n1 + n2 + n3) /3
+
+if media >= 7.0: print(f'aprovado com a media {media}')
+else: print(f'reprovado com a media {media}')
+
+
