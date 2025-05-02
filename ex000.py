ffc848e6e959dbf442d538658b2cad6c3b22c51d
Quinha10 <lucasduarte2608@gmail.com>
2025-05-02 01:31:34 -0300

    Inicio do estudo dia1

--- ex000.py
+++ ex000.py
@@ -1,11 +1,1 @@
- #Ex-soma-python
-n1 = float (input('digite a nota1:'))
-n2 = float (input('digita a nota2:'))
-n3 = float (input('digita a nota3:'))
-
-media = (n1 + n2 + n3) /3
-
-if media >= 7.0: print(f'aprovado com a media {media}')
-else: print(f'reprovado com a media {media}')
-
-
+print ('Olá mundo!')
